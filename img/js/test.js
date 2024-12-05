40c48b54c90b11187eba312cdc86a2cf38f752b9
DevDavid06 <iamjaybest@gmail.com>
2024-12-05 16:31:01 +0100

    Add files via upload

---
+++ img/js/test.js
@@ -0,0 +1,12 @@
+const array = ['volvo', 'saab', 'lambo'];
+array[0] = 'audi';
+console.log(array)
+
+const d = new Date();
+console.log(d.getMonth() +1)
+console.log(d.getDay() +1)
+console.log(Math.PI);
+console.log(Math.random())
+//assignment
+
+
